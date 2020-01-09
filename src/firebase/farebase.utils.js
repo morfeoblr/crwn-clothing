1fe86c52e627b3db85e0f6a2bfbcba19dc4d0824
Aliaksei Bakhur <a.bakhur@godeltech.com>
2020-01-09 13:25:48 +0300

    added styled component module and updates code accordingly for some components, added new method into firebase util to get stored some stuff

--- src/firebase/farebase.utils.js
+++ src/firebase/farebase.utils.js
@@ -3,43 +3,55 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDdKPCAfjN_jbAKYeLfrKgdujTxTtalA6I",
-    authDomain: "crwn-db-6cacb.firebaseapp.com",
-    databaseURL: "https://crwn-db-6cacb.firebaseio.com",
-    projectId: "crwn-db-6cacb",
-    storageBucket: "crwn-db-6cacb.appspot.com",
-    messagingSenderId: "971558514268",
-    appId: "1:971558514268:web:c80669ee587899947f0099",
-    measurementId: "G-9XF96VD2V0"
+  apiKey: "AIzaSyDdKPCAfjN_jbAKYeLfrKgdujTxTtalA6I",
+  authDomain: "crwn-db-6cacb.firebaseapp.com",
+  databaseURL: "https://crwn-db-6cacb.firebaseio.com",
+  projectId: "crwn-db-6cacb",
+  storageBucket: "crwn-db-6cacb.appspot.com",
+  messagingSenderId: "971558514268",
+  appId: "1:971558514268:web:c80669ee587899947f0099",
+  measurementId: "G-9XF96VD2V0"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapShot = await userRef.get();
+  const snapShot = await userRef.get();
 
-    if (!snapShot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            });
-        } catch (error) {
-            console.log('error creating user', error.message);
-        }
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
     }
+  }
 
-    return userRef;
+  return userRef;
 }
 
-firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
